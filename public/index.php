75968d892439bc16bdfadafeb70e5c48fc98487b
Hiago <hiago@inovadora.com.br>
2018-05-29 22:14:37 -0300

    adicionado autoloader

--- public/index.php
+++ public/index.php
@@ -1,5 +1,24 @@
 <?php
 
-chdir(dirname(__DIR__));
+define('APP_PATH', __DIR__ . '/../');
 
-include 'src/index.php';
+include APP_PATH . 'library/Ps4AutoloaderClass.php';
+
+$loader = new Psr4AutoloaderClass();
+
+$loader->addNamespace('classes','src');
+$loader->register();
+
+/*
+set_include_path(get_include_path().PATH_SEPARATOR.__DIR__.'/../library/PHPMailer');
+
+function __autoload($classname){
+    echo $classname;
+}
+
+function autoloadApp($classname){
+    include "src/classes/$classname.php";
+} 
+*/
+
+include APP_PATH . 'src/index.php';
